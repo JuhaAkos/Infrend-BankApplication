46801de4720d5e41f473d9c4e9d3641d0ff33b2f
JuhaAkos <juhaszakos7@gmail.com>
2023-05-28 01:31:03 +0200

    transaction list, form

--- src/app/transaction-list/transaction-list.component.ts
+++ src/app/transaction-list/transaction-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../services/account.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { AccountDTO, TransactionDTO } from 'models';
 import { TransactionService } from '../services/transaction.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-transaction-list',
@@ -11,18 +13,35 @@ import { TransactionService } from '../services/transaction.service';
 })
 export class TransactionListComponent {
   constructor(   
+    private formBuilder: FormBuilder,
     private accountService: AccountService,
     private transactionService: TransactionService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private toastrService: ToastrService,
     ) { }
 
+  shownForm = "none";
   account?: AccountDTO;
   senderTransactions: TransactionDTO[] =[];
   receiverTransactions: TransactionDTO[] =[];
-  allTransactions: TransactionDTO[] = []; 
+  allTransactions: TransactionDTO[] = [];
+  accounts: AccountDTO[] = [];
 
   sendershown = false;
+
+  withDrawalForm = this.formBuilder.group({
+    amount: this.formBuilder.control(0)
+  });  
+
+  depositForm = this.formBuilder.group({
+    amount: this.formBuilder.control(0)
+  }); 
+
+  standardForm = this.formBuilder.group({
+    amount: this.formBuilder.control(0),
+    receiver: ['', [Validators.required]],
+    description: this.formBuilder.control(''),
+  }); 
   
   ngOnInit(): void {
       const id = this.activatedRoute.snapshot.params['id'];
@@ -30,6 +49,19 @@ export class TransactionListComponent {
       this.accountService.getOne(id).subscribe({
         next: (account) => {
           this.account = account;
+
+          this.accountService.getAll().subscribe({
+            next: (accounts) => {
+                this.accounts = accounts;
+                for(let i=0; i<this.accounts.length; i++){
+                  
+                  if (this.accounts[i].id == this.account?.id) {
+                    
+                    this.accounts.splice(i, 1);
+                  }
+                }
+            }
+          });
 
           //osszes tranzakcio betolt
           this.transactionService.getAll().subscribe({
@@ -52,7 +84,7 @@ export class TransactionListComponent {
           }
           });  
         }
-      });          
+      });       
     }
 
   showSender() {    
@@ -63,4 +95,133 @@ export class TransactionListComponent {
     this.sendershown=false; 
   }
   
+  openFormStandard() {
+    if (this.shownForm != "standard") {
+      this.shownForm = "standard";
+    } else {
+      this.shownForm = "none";
+    }    
+  }
+
+  openFormWithDrawal() {
+    if (this.shownForm != "withdrawal") {
+      this.shownForm = "withdrawal";
+    } else {
+      this.shownForm = "none";
+    }   
+  }
+
+  openFormDeposit() {
+    if (this.shownForm != "deposit") {
+      this.shownForm = "deposit";
+    } else {
+      this.shownForm = "none";
+    }   
+  }
+
+  saveWithDrawal(){
+    const transaction = this.withDrawalForm.value as TransactionDTO;
+
+    transaction.date = new Date();
+    if (this.account != undefined ){
+      transaction.sender = this.account;
+    }    
+    transaction.receiver = null;
+    transaction.description = "Banki kifizetés";
+
+    if (this.account != undefined) {
+      var newBalance = this.account.balance-transaction.amount;
+      if (newBalance<0){
+        this.toastrService.error('Egyenleg túllépés');
+        return;
+      }
+      this.account.balance = newBalance;
+      this.updateBalance(this.account);
+    }
+
+    this.createTransaction(transaction);
+  }
+
+  saveDeposit(){
+    const transaction = this.depositForm.value as TransactionDTO;
+
+    transaction.date = new Date();
+    if (this.account != undefined ){
+      transaction.receiver = this.account;
+    }    
+    transaction.sender = null;
+    transaction.description = "Banki befizetés";
+
+    if (this.account != undefined) {
+      var newBalance = +this.account.balance + +transaction.amount;     
+      
+      this.account.balance = newBalance;
+      this.updateBalance(this.account);
+    }
+
+    this.createTransaction(transaction);
+  }
+
+  saveStandard(){
+    const transaction = this.standardForm.value as TransactionDTO;    
+
+    transaction.date = new Date();
+    if (this.account != undefined ){
+      transaction.sender = this.account;
+    } 
+
+    if (this.account != undefined) {
+      var newBalance = this.account.balance-transaction.amount;
+      if (newBalance<0){
+        this.toastrService.error('Egyenleg túllépés');
+        return;
+      }      
+      
+      if (transaction.receiver != undefined) {
+        if (transaction.receiver.status == "inactive") {
+          this.toastrService.error('A címzett számla inaktív');
+          return;
+        }
+        console.log(transaction.receiver.balance);
+        transaction.receiver.balance += +transaction.amount;
+        this.updateBalance(transaction.receiver);
+        console.log(transaction.receiver.balance);
+      }
+
+      this.account.balance = newBalance;
+      this.updateBalance(this.account);
+    }
+
+      
+    
+    this.createTransaction(transaction);
+  }
+
+  createTransaction(transaction: TransactionDTO){
+    this.transactionService.create(transaction).subscribe({
+      next: (transaction) => {
+        this.toastrService.success('Tranzakció hozzáadva, id:' + transaction.id , 'Siker');
+        window.location.reload();
+      },
+      error: (err) => { 
+        this.toastrService.error('Tranzakció hozzáadása sikertleen');
+      }
+    });
+  }
+
+  updateBalance(account: AccountDTO){
+    if (account != undefined) {
+      this.accountService.update(account).subscribe({
+        next: (account) => {}
+      });
+    }    
+  }
+
+  isItInactive(): boolean {
+    if (this.account?.status=="inactive") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
